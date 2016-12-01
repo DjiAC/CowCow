2179e5acc75ac9b54a47675c998c9e9c0099325a
Dr Naabs <Naabs@CHARLES>
2016-12-01 13:32:55 +0100

    J'ai mis le type Carte et Cartes spéciale, il faudra mettre les méthodes pour les cartes spéciales.

--- Mow/Mow.Core/CarteSpeciale.cs
+++ Mow/Mow.Core/CarteSpeciale.cs
@@ -5,12 +5,22 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Mow.Core
-{
+{    
+    
+    /// <summary>
+    /// Une carte spéciale est défini par son type et les méthodes associé à son type
+    /// </summary>
+          
     class CarteSpeciale : Carte
     {
     public enum TypeDeCarteSpecial {Acrobate, Retardataire, SerreFile  }   // type de carte spécial par C
 
     //TODO Méethodes des cartes spéciales
+
+    public void Acrobate
+        {
+
+        }
 
     }
 }
