5145bfaa4077ca0e445d6cd197549caf3efee5c0
Dr Naabs <Naabs@CHARLES>
2016-12-08 14:34:29 +0100

    Modif Carte

--- Mow/Mow.Core/CarteSpeciale.cs
+++ Mow/Mow.Core/CarteSpeciale.cs
@@ -8,7 +8,7 @@ namespace Mow.Core
 {
 
     /// <summary>
-    /// Une carte spéciale est défini par son type et les méthodes associé à son type
+    /// Une carte spéciale est défini par son type et les méthodes associé à son type.
     /// </summary>
 
     class CarteSpeciale : Carte
@@ -16,12 +16,21 @@ namespace Mow.Core
 
         //TODO Méethodes des cartes spéciales
 
+
+
+        /// <summary>
+        /// Une vache acrobate se met par dessus d'une autre qui porte le même numéro.
+        /// </summary>
+        /// <param name="Type"></param>
         public void Acrobate(TypeDeCarte Type)
         {
 
         }
 
-
+        /// <summary>
+        /// Une vache retardataire se met entre deux vaches dont l'écart des numéro est de 2 minimum.
+        /// </summary>
+        /// <param name="Type"></param>
         public void Retardataire(TypeDeCarte Type)
         {
 
--- Mow/Mow.Core/Partie.cs
+++ Mow/Mow.Core/Partie.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
 
 namespace Mow.Core
 {
@@ -14,7 +15,31 @@ namespace Mow.Core
                 var MaManche = new Manche(i);
             }
         }
-        public List<Joueur> Joueurs = new List<Joueur>();
+
+        public void Json() { 
+        var json = System.IO.File.ReadAllText(@"C:\Users\Charles\Downloads\cartes.json"); // à modifier mettre la source
+
+        var objects = JArray.Parse(json); // parse as array  
+            foreach (JObject root in objects)
+            {
+                foreach (KeyValuePair<String, JToken> app in root)
+                {
+                    var appName = app.Key;
+                    var type = (String)app.Value["Type"];
+                    var nombre = (String)app.Value["Nombre"];
+                    var mouche = (String)app.Value["Mouches"];
+
+                    Console.WriteLine(appName);
+                    Console.WriteLine(type);
+                    Console.WriteLine(nombre);
+                    Console.WriteLine(mouche);
+                    Console.WriteLine("\n");
+
+                }
+            }
+}
+
+public List<Joueur> Joueurs = new List<Joueur>();
         
     }
 }
