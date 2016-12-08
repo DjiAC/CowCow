641b82bb9debdb6323fcdb8cc8fe84abd9b397fe
sayeng <guillaume.sayen@epsi.fr>
2016-12-08 14:16:47 +0100

    ne pas prendre en compte

---
+++ Mow/Mow.Core/sqlite.cs
@@ -0,0 +1,17 @@
+﻿using SQLite.Net;
+using SQLite.Net.Platform.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mow.Core
+{
+    class sqlite
+    {
+            var db = new SQLiteConnection(new SQLitePlatformWin32(), "c://temp//mydb.sqlite");
+            db.CreateTable<Statscore>();
+            db.CreateTable<Statjoueur>();
+    }
+}
